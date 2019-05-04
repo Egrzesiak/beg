f907dd9f1fc79c572a2a853d3c4a2da5f3520991
Emil Grzesiak <sztormiks@gmail.com>
2019-05-04 13:11:05 +0200

    some comm

--- resources/views/urlop.blade.php
+++ resources/views/urlop.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Zar-Kom
+                    Tu będą urlopy
                 </div>
 
                 <div class="links">
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('urlop', function () {
+    return view('urlop');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
